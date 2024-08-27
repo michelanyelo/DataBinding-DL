0e7619be6c1d4fb37076cdbb8d40a16576ba90d3
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-27 19:19:25 -0400

    M6D6 desafio: Removed empty lines and added DataBinding component import and usage in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-
+import DataBinding from './components/DataBinding.vue'
 </script>
 
 <template>
-
+    <DataBinding />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'DataBinding',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<template>
+    <div class="container">
+        <div class="selectores">
+
+        </div>
+        <div class="resultados">
+
+        </div>
+    </div>
+</template>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+    max-width: 100%;
+    margin: 0 auto;
+}
+</style>
