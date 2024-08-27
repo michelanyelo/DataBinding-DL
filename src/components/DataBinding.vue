26c71c1dbe30851136b613ca1292fac796dc00ba
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-27 19:27:15 -0400

    M6D6 desafio: Removed border property from base.css, added backgroundBox property and input field to DataBinding.vue, and updated styles for container, selectores, and box elements.

--- src/components/DataBinding.vue
+++ src/components/DataBinding.vue
@@ -3,17 +3,19 @@ export default {
     name: 'DataBinding',
     data() {
         return {
-
+            backgroundBox: ''
         }
     }
 }
 </script>
+
 <template>
     <div class="container">
         <div class="selectores">
-
+            <label for="backgroundBox">Color de fondo: </label>
+            <input id="backgroundBox" type="text" v-model="backgroundBox">
         </div>
-        <div class="resultados">
+        <div class="box" :style="{ backgroundColor: backgroundBox }">
 
         </div>
     </div>
@@ -22,11 +24,28 @@ export default {
 <style>
 .container {
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-direction: row;
+    align-items: flex-start;
     gap: 10px;
     width: 100%;
     max-width: 100%;
     margin: 0 auto;
+}
+
+.selectores,
+.box {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+.selectores {
+    justify-content: center;
+}
+
+.box {
+    height: 100px;
+    width: 100px;
+    border: 1px;
 }
 </style>
